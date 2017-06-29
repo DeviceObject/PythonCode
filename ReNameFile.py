52749efe6da91ec4d0331441d17be81dfa898cc6
PassP <deviceobject@gmail.com>
2017-06-29 09:03:52 +0800

    Update ReNameFile.py
    VS Build Post-event CommandLine : python C:\BkManager\Bin\ToTimeFile.py C:\BkManager\Bin\Setup.exe C:\BkManager\Bin,
    Generate file's filename to time format.

--- ReNameFile.py
+++ ReNameFile.py
@@ -3,10 +3,10 @@ import datetime
 import shutil
 
 def entry():
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         now = datetime.datetime.now()
         newFile = now.strftime('%Y-%m-%d-%H-%M-%S.Exe')
-        shutil.move(sys.argv[1],newFile)
+        shutil.move(sys.argv[1],sys.argv[2] + '\\' + newFile)
         print(sys.argv[1] + " rename " + newFile)
 if __name__ == "__main__":
     entry()
