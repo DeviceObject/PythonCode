1e2d544674a1bb1db7753ba193298118f3d23c49
deviceobject <deviceobject@gmail.com>
2017-07-04 17:00:26 +0800

    fixed show banner

--- showbanner.py
+++ showbanner.py
@@ -10,16 +10,16 @@ def retBanner(ip, port):
 	except:
 		return
 def showBanner(banner):
-	print bannern
+	print banner
 def main():
-	for x in range(1,255):
+	for x in range(36,255):
 		ip = "192.168.50." + str(x)
 		for port in range(20,65535):
 			banner = retBanner(ip, port)
 			if banner:
-				print '[+] ' + ip + ': ' + str(port)
+				print '[+] ' + ip + ': ' + str(port) + 'y'
 				showBanner(banner)
-			else:
-				print '[+] ' + ip + ': ' + str(port)
+			#else:
+			#	print '[+] ' + ip + ': ' + str(port) + 'n'
 if __name__ == '__main__':
 	main()
