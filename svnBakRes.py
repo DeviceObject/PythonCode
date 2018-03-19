28001268e576888f5e0c94ee19b5236eade8c91d
PassP <deviceobject@gmail.com>
2018-03-19 14:43:48 +0800

    update svn bak code
    add import all  .svn .bak

--- svnBakRes.py
+++ svnBakRes.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import string
 
 def entry():
     if (len(sys.argv) < 3):
@@ -7,9 +8,30 @@ def entry():
     if (sys.argv[1] == "export"):
         exportSvnBak(sys.argv[2],sys.argv[3])
     elif (sys.argv[1] == "import"):
-        importSvnBak(sys.argv[2],sys.argv[3])
+        if (sys.argv[2] == "all"):
+            importAllBak(sys.argv[3],sys.argv[4])
+        else:
+            importSvnBak(sys.argv[2],sys.argv[3])
     else:
         print("exit()")
+def importAllBak(svnRepositoriesPath,svnBakPath):
+    if (os.path.exists(svnRepositoriesPath) == False):
+        print("Repositories path isn't exist.")
+    if (os.path.exists(svnBakPath) == False):
+        print("Svn bak path isn't exist.")
+    allDir = os.listdir(svnBakPath)
+    for svnLibName in allDir:
+        nOffsetIndex = svnLibName.find(".")
+        if (nOffsetIndex):
+            newSvnRepositoriesPath = svnRepositoriesPath + "\\" + svnLibName[0:nOffsetIndex]
+            if (os.path.exists(newSvnRepositoriesPath) == False):
+                continue            
+            newSvnBakPath = svnBakPath + "\\" + svnLibName
+            if (os.path.exists(newSvnBakPath) == False):
+                continue
+            command = "svnadmin" + " " + "load" + " " + newSvnRepositoriesPath + " < " + newSvnBakPath
+            print(command)
+            os.system(command)
 def importSvnBak(svnRepositoriesPath,svnBakPath):
     if (os.path.exists(svnRepositoriesPath) == False):
         print("Repositories path isn't exist.")
