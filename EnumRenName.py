b0e02ffd4fd3ec72695a3570dfd6f35ab6276d85
PassP <deviceobject@gmail.com>
2018-06-14 12:22:36 +0800

    update
    update

--- EnumRenName.py
+++ EnumRenName.py
@@ -33,12 +33,39 @@ def backDirectory(directory,copypath):
                     count = count + 1
                     print (count, filepath, destfile)
                     
+def scanFiles(directory):
+    for root, xdirectory, paths in os.walk(directory):
+        for path in paths:
+            filename, filesuffix = os.path.splitext(path)
+            print(filename, filesuffix)
+def copyfilestodirectory(filepath, directory):
+    countentries = 0;
+    if os.path.exists(directory) == False:
+        print(directory + " isn't exist")
+        os.makedirs(directory)
+        print("create directory " + directory)
+    if os.path.isdir(filepath):
+        for root, xdirectory, paths in os.walk(filepath):
+            for path in paths:
+                filename, filesuffix = os.path.splitext(path)
+                sourcefile = root + "\\" + path;
+                destfile = directory + "\\" + path
+                if os.access(sourcefile, os.R_OK):
+                    print(countentries, sourcefile, destfile)   
+                    shutil.copy(sourcefile, destfile)
+                    countentries = countentries + 1
+                else:
+                    print(sourcefile + " isn't access")
+    else:
+        print(filepath + "isn't a directory")
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         exit()
-    if os.path.exists(sys.argv[1]):
-        backDirectory(sys.argv[1],sys.argv[2])
+    #if os.path.exists(sys.argv[1]):
+    #    backDirectory(sys.argv[1],sys.argv[2])
     #if os.path.exists(sys.argv[1]):
     #    EnumDirectory(sys.argv[1],sys.argv[2])
-    else:
-        print("Path" + "\"" + sys.argv[1] + "\"" + "isn't Directory")
+    #scanFiles(sys.argv[1])
+    copyfilestodirectory(sys.argv[1],sys.argv[2])
+    #else:
+    #   print("Path" + "\"" + sys.argv[1] + "\"" + "isn't Directory")
